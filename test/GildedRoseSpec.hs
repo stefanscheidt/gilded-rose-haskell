98d69e9375ffa134bb4a3fc0960b93dadf332fec
Stefan Scheidt <stefan.scheidt@rewe-digital.com>
2018-01-11 22:26:50 +0100

    first tests

--- test/GildedRoseSpec.hs
+++ test/GildedRoseSpec.hs
@@ -7,8 +7,26 @@ spec :: Spec
 spec =
   describe "updateQuality" $ do
 
-    it "fixme" $
-       let inventory = [Item "foo" 0 0]
-           actual = updateQuality inventory
-           expected = []
-       in actual `shouldBe` expected
+    it "decrements sellIn and quality" $
+      let inventory = [Item "item" 10 10]
+          actual = updateQuality inventory
+          expected = [Item "item" 9 9]
+      in actual `shouldBe` expected
+
+    it "never decrements quality below zero" $
+      let inventory = [Item "item" 10 0]
+          actual = updateQuality inventory
+          expected = [Item "item" 9 0]
+      in actual `shouldBe` expected
+
+    it "decrements quality twice after sellIn passed" $
+      let inventory = [Item "item" 0 10]
+          actual = updateQuality inventory
+          expected = [Item "item" (-1) 8]
+      in actual `shouldBe` expected
+
+    it "decrement quality twice honors quality being non negative" $
+      let inventory = [Item "item" 0 1]
+          actual = updateQuality inventory
+          expected = [Item "item" (-1) 0]
+      in actual `shouldBe` expected
