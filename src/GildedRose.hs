df7f1662b59aef4c3ea1a2e4b9990aee9cef1bd2
Stefan Scheidt <stefan.scheidt@rewe-digital.com>
2018-01-14 20:57:37 +0100

    More refactorings.

--- src/GildedRose.hs
+++ src/GildedRose.hs
@@ -9,6 +9,23 @@ instance Show Item where
   show (Item name sellIn quality) =
     name ++ ", " ++ show sellIn ++ ", " ++ show quality
 
+updateQuality :: GildedRose -> GildedRose
+updateQuality = map updateQualityItem
+
+updateQualityItem :: Item -> Item
+updateQualityItem item@(Item name sellIn quality) =
+  if name == "Aged Brie"
+  then
+    updateQualityCheese item
+  else if name == "Backstage passes to a TAFKAL80ETC concert"
+  then
+    updateQualityBackstagePasses item
+  else if name == "Sulfuras, Hand of Ragnaros"
+  then
+    updateQualityLegendary item
+  else
+    updateQualityRegular item
+
 updateQualityCheese :: Item -> Item
 updateQualityCheese (Item name sellIn quality) =
   let
@@ -20,63 +37,50 @@ updateQualityCheese (Item name sellIn quality) =
       then
           if quality' < 50
           then (Item name sellIn' (quality' + 1))
-          else (Item name sellIn' quality')
+          else (Item name sellIn' quality)
       else (Item name sellIn' quality')
-    
-updateQualityItem :: Item -> Item
-updateQualityItem item@(Item name sellIn quality) =
-  if name == "Aged Brie"
-  then
-    updateQualityCheese item
-  else
-    let
-      quality' =
-        if name /= "Backstage passes to a TAFKAL80ETC concert"
+
+updateQualityBackstagePasses :: Item -> Item
+updateQualityBackstagePasses (Item name sellIn quality) =
+  let
+    quality' =
+      if quality < 50
         then
-          if quality > 0
-          then
-            if name /= "Sulfuras, Hand of Ragnaros"
-            then quality - 1
-            else quality
-          else quality
-        else
-          if quality < 50
-          then
-            quality + 1 +
-              (if name == "Backstage passes to a TAFKAL80ETC concert"
+          quality + 1 +
+              if sellIn < 11
               then
-                if sellIn < 11
+                if quality < 49
                 then
-                  if quality < 49
-                  then
-                    1 + (if sellIn < 6
-                          then
-                            if quality < 48
-                            then 1
-                            else 0
-                          else 0)
-                  else 0
+                  1 + (if sellIn < 6
+                        then
+                          if quality < 48
+                          then 1
+                          else 0
+                        else 0)
                 else 0
-              else 0)
-          else quality
+              else 0
+        else quality
 
-      sellIn' =
-        if name /= "Sulfuras, Hand of Ragnaros"
-        then sellIn - 1
-        else sellIn
-    in
-      if sellIn' < 0
+    sellIn' = sellIn - 1
+  in
+    if sellIn' < 0
+    then (Item name sellIn' 0)
+    else (Item name sellIn' quality')
+
+updateQualityLegendary :: Item -> Item
+updateQualityLegendary item = item
+
+updateQualityRegular :: Item -> Item
+updateQualityRegular (Item name sellIn quality) =
+  let
+    quality' | quality > 0 = quality - 1
+             | otherwise   = quality
+    sellIn' = sellIn - 1
+  in
+    if sellIn' < 0
+    then
+      if quality' > 0
       then
-        if name /= "Backstage passes to a TAFKAL80ETC concert"
-        then
-          if quality' > 0
-          then
-            if name /= "Sulfuras, Hand of Ragnaros"
-            then (Item name sellIn' (quality' - 1))
-            else (Item name sellIn' quality')
-          else (Item name sellIn' quality')
-        else (Item name sellIn' (quality' - quality'))
+        (Item name sellIn' (quality' - 1))
       else (Item name sellIn' quality')
-
-updateQuality :: GildedRose -> GildedRose
-updateQuality = map updateQualityItem
+    else (Item name sellIn' quality')
